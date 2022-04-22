bb74de2ae686180ff6cd29d7f73d984ce69dec18
alisaffak <75166178+alisaffak@users.noreply.github.com>
2022-04-22 16:30:05 +0300

    Update mainwindow.h

--- SystemMonitor/mainwindow.h
+++ SystemMonitor/mainwindow.h
@@ -18,28 +18,20 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
     SystemInfo m_systemInfo;
 
 private:
     void draw();
     void resizeEvent(QResizeEvent* event);
-
     QMutex m_mutex;
-
-
 
 public slots:
     void slotTimer();
-
-
+    
 private:
     Ui::MainWindow *ui;
     QTimer m_timer;
     QVector<int> m_cpuUsage;
     QGraphicsScene* m_scene;
-
-
-
 };
 #endif // MAINWINDOW_H
