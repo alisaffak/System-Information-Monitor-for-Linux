8da11109ac55a84a1bf712240249aa34d805d540
alisaffak <75166178+alisaffak@users.noreply.github.com>
2022-04-22 16:24:33 +0300

    Update main.cpp

--- SystemMonitor/main.cpp
+++ SystemMonitor/main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w; 
     w.setWindowIcon(QIcon("/home/ali/SystemMonitor/icon/system.ico") );
-
     w.show();
     return a.exec();
 }
