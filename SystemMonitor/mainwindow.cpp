b4690beec3127cfa54491745454e8316bd69ee49
alisaffak <75166178+alisaffak@users.noreply.github.com>
2022-04-22 16:29:09 +0300

    Update mainwindow.cpp

--- SystemMonitor/mainwindow.cpp
+++ SystemMonitor/mainwindow.cpp
@@ -33,13 +33,11 @@ void MainWindow::draw()
         return;
 
     int graphSize = 60;
-
-
-
+    
     QSize sizeofchart = ui->graphicsView->size();
-
     QImage img(sizeofchart, QImage::Format_RGB888);
     img.fill(Qt::gray);
+    
     QPainter painter(&img);
     QPen pen;
 
@@ -49,25 +47,20 @@ void MainWindow::draw()
     painter.drawLine(0, img.height()*0.25, img.width(), img.height()*0.25);
     painter.drawLine(0, img.height()*0.5, img.width(), img.height()*0.5);
     painter.drawLine(0, img.height()*0.75, img.width(), img.height()*0.75);
-//    painter.drawText(0,img.height()*0.95,"%0");
     painter.drawText(0,img.height()*0.75,"%25");
     painter.drawText(0,img.height()*0.5,"%50");
     painter.drawText(0,img.height()*0.25,"%75");
-//    painter.drawText(0,img.height()*0.05,"%100");
-
+    
     int stepx = img.width()/10;
 
-    for(int i = stepx ; i < img.width(); i = i + stepx){
-
+    for(int i = stepx ; i < img.width(); i = i + stepx)
+    {
         painter.drawLine(i, 0, i, img.height());
     }
-
-
-
+    
     pen.setWidth(2);
     pen.setColor(Qt::blue);
     painter.setPen(pen);
-
 
     float stepHorizontal = (float)img.width() / (float)graphSize;
     float stepVertical = (float)img.height() / 100.f;
@@ -78,32 +71,18 @@ void MainWindow::draw()
     for(int i=0; i<m_cpuUsage.size(); i++)
     {
         p1 = QPoint((i+1)*stepHorizontal, img.height() - (float)m_cpuUsage.at(i)*stepVertical);
-
         painter.drawLine(previous, p1);
         previous = p1;
-
     }
-
-
-
-
-//    qDebug()<<"last point"<< p1;
-//    qDebug()<<"cpu list"<<m_cpuUsage;
-//    qDebug()<<"---------------------------------";
-
-
+    
     painter.end();
 
-
-
     QGraphicsPixmapItem* item = new QGraphicsPixmapItem(QPixmap::fromImage(img));
+
     m_scene->clear();
     m_scene->setSceneRect(0, 0, sizeofchart.width(), sizeofchart.height());
     m_scene->addItem(item);
-
     ui->graphicsView->setScene(m_scene);
-//    ui->graphicsView->baseSize();
-
 }
 
 void MainWindow::resizeEvent(QResizeEvent *event)
@@ -118,24 +97,24 @@ void MainWindow::slotTimer()
     float cpuInfo = m_systemInfo.getCpuValue();
 
     m_cpuUsage.append(cpuInfo);
+    
     if(m_cpuUsage.size() > 60)
         m_cpuUsage.removeFirst();
-
-
+    
     draw();
 
     // STORAGE
     qint64 bytesTotal, bytesAvailable, bytesFree;
+    
     if( m_systemInfo.getSdaInfo(bytesTotal, bytesAvailable, bytesFree) )
     {
         float percent = 100.f * (float)bytesFree / (float)bytesTotal;
         ui->pbSda->setValue(percent);
     }
-
     // MEMORY
     int total,used,free;
-    if(m_systemInfo.getMemoryInfo(total,free,used)){
-
+    if(m_systemInfo.getMemoryInfo(total,free,used))
+    {
         int percent = 100.f * (int)used / (int)total;
         ui->pbRam->setValue(percent);
     }
@@ -145,12 +124,5 @@ void MainWindow::slotTimer()
     ui->lb_TotalSdaValue->setText(QString::number(bytesTotal/1000/1000/1000)+" GB");
     ui->lb_UsedSdaVAlue->setText(QString::number(bytesAvailable/1000/1000/1000)+" GB");
     ui->label_cpu->setText(QString::number(cpuInfo));
-
-
-
-
-
-
-
 }
 
